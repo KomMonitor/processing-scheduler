299fa17676aff4b77917daf2e4462a1627d39610
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-05-02 20:18:04 +0200

    add disable logs option

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 
+if(JSON.parse(process.env.DISABLE_LOGS)){
+    console.log = function(){};
+}
+
 // import cron from "node-cron";
 const cron = require("node-cron");
 const indicatorProcessHelper = require("./indicatorProcessHelper/indicatorProcessHelper");
 
 // run every day at 23:00 hours
-cron.schedule("*/10 * * * * *", () => {
+cron.schedule(" 20 0 * * *", () => {
   console.log(`Begin check to find schedulable indicator processes`);
   
   indicatorProcessHelper.triggerIndicatorComputationForMissingTimestamps();
