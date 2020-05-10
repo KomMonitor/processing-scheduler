c568213f427934aa836868b9347da0adaed4e6ec
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-05-11 00:38:10 +0200

    set run schedule to 00:30 daily

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ if(JSON.parse(process.env.DISABLE_LOGS)){
 const cron = require("node-cron");
 const indicatorProcessHelper = require("./indicatorProcessHelper/indicatorProcessHelper");
 
-// run every day at 23:00 hours
-cron.schedule(" 20 0 * * *", () => {
+// run every day at 00:30 hours
+cron.schedule("0 30 0 1/1 * ? *", () => {
   console.log(`Begin check to find schedulable indicator processes`);
   
   indicatorProcessHelper.triggerIndicatorComputationForMissingTimestamps();
