3498e4c3ff34240d842f44ef5badea30cf6381e6
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-05-11 10:42:25 +0200

    make cron patten configurable

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ if(JSON.parse(process.env.DISABLE_LOGS)){
     console.log = function(){};
 }
 
+var cronPattern = process.env.CRON_PATTERN_FOR_SCHEDULING;
+
 // import cron from "node-cron";
 const cron = require("node-cron");
 const indicatorProcessHelper = require("./indicatorProcessHelper/indicatorProcessHelper");
 
+console.log("Initialize with cron schedule '" + cronPattern + "'");
+
 // run every day at 00:30 hours
-cron.schedule("0 30 0 1/1 * ? *", () => {
-  console.log(`Begin check to find schedulable indicator processes`);
+cron.schedule(cronPattern, () => {
+  console.log("Begin check to find schedulable indicator processes");
   
   indicatorProcessHelper.triggerIndicatorComputationForMissingTimestamps();
 });
